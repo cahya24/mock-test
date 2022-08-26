7ff7f424de089b6519b39b1042b3a2151fa1e119
cahya24 <cahya.butex@yahoo.com>
2022-08-26 14:15:49 +0700

    Input Optimization + Cooldown System

--- champion_list.py
+++ champion_list.py
@@ -4,18 +4,34 @@ class Fizz(Champion):
     def __init__(self, attack_damage, ability_power, health, mana, level, attack_range, cooldown):
         super().__init__(attack_damage, ability_power, health, mana, level, attack_range)
         self.cooldown = cooldown
+    
+    def basic_attack(self, target):
+        damage = champ_fizz.attack_damage
+        target.health -= damage
+        print(f"Fizz dealt {damage} damage to Brand")
 
     def use_q_ability(self, target):
-        target.health -= int(120 + (champ_fizz.ability_power * 1.2))
+        damage = int(120 + (champ_fizz.ability_power * 1.2))
+        target.health -= damage
+        self.cooldown = 5
+        print(f"Fizz dealt {damage} damage to Brand, Cooldown for 5 seconds")
 
-champ_fizz = Fizz(60,0,800,400,1,125)
+champ_fizz = Fizz(60,0,800,400,1,125,0)
 
 class Brand(Champion):
     def __init__(self, attack_damage, ability_power, health, mana, level, attack_range, cooldown):
         super().__init__(attack_damage, ability_power, health, mana, level, attack_range)
         self.cooldown = cooldown
+
+    def basic_attack(self, target):
+        damage = champ_brand.attack_damage
+        target.health -= damage
+        print(f"Brand dealt {damage} damage to Fizz")
     
     def use_q_ability(self, target):
-        target.health -= int(200 + (champ_brand.ability_power * 1.1))
+        damage = int(200 + (champ_brand.ability_power * 1.1))
+        target.health -= damage
+        self.cooldown = 8
+        print(f"Brand dealt {damage} damage to Fizz, Cooldown for 8 seconds")
 
-champ_brand = Brand(70,0,750,350,1,600)
+champ_brand = Brand(70,0,750,350,1,600,0)
--- main.py
+++ main.py
@@ -3,4 +3,45 @@ from champion_list import*
 import time
 
 if __name__ == "__main__":
+    champ_fizz.displaystat()
+    champ_fizz.levelup()
+    champ_fizz.displaystat()
+    print("List of Champions:")
+    print("Brand")
+    print("Fizz")
+    action = input("Please select your champion: ")
+    if action == "Brand":
+        print("-" * 99)
+        print("Basic Attack")
+        print("Use Q Ability")
+        action = input("It's your turn now, please choose an action: ")
+        if action == "Basic Attack":
+            print("-" * 99)
+            champ_brand.basic_attack(champ_fizz)
+            cooldown = 0
+        elif action == "Use Q Ability":
+            print("-" * 99)
+            champ_brand.use_q_ability(champ_fizz)
+            cooldown = champ_brand.cooldown
+    elif action == "Fizz":
+        print("-" * 99)
+        print("Basic Attack")
+        print("Use Q Ability")
+        action = input("It's your turn now, please choose an action: ")
+        if action == "Basic Attack":
+            print("-" * 99)
+            champ_fizz.basic_attack(champ_brand)
+            cooldown = 0
+        elif action == "Use Q Ability":
+            print("-" * 99)
+            champ_fizz.use_q_ability(champ_brand)
+            cooldown = champ_fizz.cooldown
+    if cooldown > 0:
+        for i in range(cooldown):
+            cooldown -= 1
+            time.sleep(1)
+            print(cooldown)
+            if cooldown == 0:
+                print("Q is now ready to use")
     champ_brand.displaystat()
+    champ_fizz.displaystat()
