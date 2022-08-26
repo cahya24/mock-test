688f196337e739fcb32362995ae3ba347460720d
cahya24 <cahya.butex@yahoo.com>
2022-08-26 13:49:14 +0700

    Created Skills for Brand

--- champion_list.py
+++ champion_list.py
@@ -12,5 +12,8 @@ champ_fizz = Fizz(60,0,800,400,1,125)
 class Brand(Champion):
     def __init__(self, attack_damage, ability_power, health, mana, level, attack_range):
         super().__init__(attack_damage, ability_power, health, mana, level, attack_range)
+    
+    def use_q_ability(self, target):
+        target.health -= int(200 + (champ_brand.ability_power * 1.1))
 
 champ_brand = Brand(70,0,750,350,1,600)
