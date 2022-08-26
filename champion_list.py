4bc7234fa39f5b129aa311582df5adc3a0b47295
cahya24 <cahya.butex@yahoo.com>
2022-08-26 13:53:18 +0700

    Added Cooldown Attribute to Each Champions

--- champion_list.py
+++ champion_list.py
@@ -1,8 +1,9 @@
 from champion import*
 
 class Fizz(Champion):
-    def __init__(self, attack_damage, ability_power, health, mana, level, attack_range):
+    def __init__(self, attack_damage, ability_power, health, mana, level, attack_range, cooldown):
         super().__init__(attack_damage, ability_power, health, mana, level, attack_range)
+        self.cooldown = cooldown
 
     def use_q_ability(self, target):
         target.health -= int(120 + (champ_fizz.ability_power * 1.2))
@@ -10,8 +11,9 @@ class Fizz(Champion):
 champ_fizz = Fizz(60,0,800,400,1,125)
 
 class Brand(Champion):
-    def __init__(self, attack_damage, ability_power, health, mana, level, attack_range):
+    def __init__(self, attack_damage, ability_power, health, mana, level, attack_range, cooldown):
         super().__init__(attack_damage, ability_power, health, mana, level, attack_range)
+        self.cooldown = cooldown
     
     def use_q_ability(self, target):
         target.health -= int(200 + (champ_brand.ability_power * 1.1))
